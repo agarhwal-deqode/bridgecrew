1ab61ddee4ebc405120b4060cb06e3133818c7d4
agarhwal-deqode <97155095+agarhwal-deqode@users.noreply.github.com>
2023-05-17 08:34:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,11 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-007855ac798b5175e"  # Change this to your desired AMI ID
+  instance_type = "t2.micro" 
+
+  tags = {
+    Name = "example-instance"
+  }
+}
